73578a04c3d5f500fcb3aa9c96f0bcf179f8ece0
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-11-16 23:29:17 +0000

    This is some AWSESOME stuff

--- lesson2/recap_if.js
+++ lesson2/recap_if.js
@@ -1,1 +1,10 @@
 // recap about if statement
+var fruit = 'banana';
+
+if (fruit == 'apple') {
+  console.log('yes its an ' + fruit);
+} else if (fruit=='banana') {
+  console.log('its a YelloW '   +           fruit);
+} else {
+  console.log('no its an ' + fruit);
+}
---
+++ lesson2\strings.js
@@ -0,0 +1,13 @@
+var month ="january";
+console.log(month);
+console.log(month.toUpperCase());
+
+var animal = "hOrSE";
+console.log(animal);
+console.log(animal.toLowerCase(   ));
+console.log(animal.repeat(3));
+
+
+
+var msg ="hello world";
+console.log(msg);
