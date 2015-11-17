b590fb24509073cfb8862ccb08f97d55328f1302
sij <sij@hotmail.co.uk>
2015-11-17 00:16:30 +0000

    Blah

--- lesson2/strings.js
+++ lesson2/strings.js
@@ -8,25 +8,6 @@ console.log(animal.toLowerCase(   ));
 console.log(animal.repeat(3));
 
 
-<<<<<<< HEAD
 
-var msg ="hello world!";
+var msg ="hello world";
 console.log(msg);
-console.log('hello'.italics());
-
-var age  = 39;
-console.log(age.toString().repeat(3));
-=======
-var msg ="HeLLo WoRlD";
-console.log(msg.toUpperCase().toLowerCase());
-console.log(msg.repeat(3).repeat(5).toLowerCase());
-
-
-var pet = "Rabbit";
-console.log(pet.replace('Ra', 'Ho').repeat(3));
-
-console.log(Number.isInteger(6));
-console.log(Number.isInteger(6.5));
-
-var age =18;
-console.log(Number.isInteger(age));
